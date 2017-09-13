a20daeeddd9a4a8abc3fcee88077fd6cc8021074
lzfliu <lzf6922583@163.com>
2017-09-13 08:23:50 +0800

    构建微服务架构（demo）

--- springCloudModule/service1/src/main/java/com/lzf/service1/Service1Application.java
+++ springCloudModule/service1/src/main/java/com/lzf/service1/Service1Application.java
@@ -9,15 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@RestController
 public class Service1Application {
-
-    @ApiOperation(value="获取项目组Sonar对应的所有Url信息")
-    @RequestMapping("/service1")
-    public  String service1(){
-        return "service1";
-    }
-
 
     public static void main(String[] args) {
         SpringApplication.run(Service1Application.class,args);
